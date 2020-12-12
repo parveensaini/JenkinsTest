1ceaceefc1ec3ef314a891840fee5cb35db75d87
Parveen Saini <pasaini01@coupang.com>
2020-12-11 17:35:33 -0800

    Initial files

---
+++ level1/level5/src/main/groovy/ExceptionParser.groovy
@@ -0,0 +1,9 @@
+class ExceptionParser {
+     static void getExceptionCategory() {
+         println('Hello World')
+     }
+
+    static void main(String[] args) {
+        getExceptionCategory()
+    }
+}
