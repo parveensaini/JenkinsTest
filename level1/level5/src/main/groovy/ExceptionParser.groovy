d1e6b701f2cba24d04817010962fbd4aca64ee65
Parveen Saini <pasaini01@coupang.com>
2020-12-15 23:46:11 -0800

    change

--- level1/level5/src/main/groovy/ExceptionParser.groovy
+++ level1/level5/src/main/groovy/ExceptionParser.groovy
@@ -1,7 +1,7 @@
 class ExceptionParser {
      static void getExceptionCategory() {
          println('Hello World'
-                 println('Hello World')
+                 println('Hello World'
      }
 
     static void main(String[] args) {
